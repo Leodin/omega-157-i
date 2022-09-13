80300f9d50aef57adaba06ba9ab624b13391209f
Leodin <thornleodin@yahoo.com>
2022-09-13 16:01:07 +0100

    Improved error handling logic

--- analysis.py
+++ analysis.py
@@ -4,3 +4,4 @@ Temporary commit content for 2022-07-25T16:01:07
 Temporary commit content for 2022-08-26T16:01:07
 Temporary commit content for 2022-09-04T16:01:07
 Temporary commit content for 2022-09-05T16:01:07
+Temporary commit content for 2022-09-13T16:01:07
