0cac0d4a486c3dca0eaea83c5953dfdc2901a49c
Leodin <thornleodin@yahoo.com>
2022-09-24 16:01:07 +0100

    Added validation for user input

--- script.ps1
+++ script.ps1
@@ -8,3 +8,4 @@ Temporary commit content for 2022-08-07T16:01:07
 Temporary commit content for 2022-08-24T16:01:07
 Temporary commit content for 2022-09-09T16:01:07
 Temporary commit content for 2022-09-16T16:01:07
+Temporary commit content for 2022-09-24T16:01:07
