0840fd7ae35f3ce52608a18332606fcf69214c5b
Leodin <thornleodin@yahoo.com>
2023-01-18 16:01:07 +0100

    Updated README with new features

--- analysis.py
+++ analysis.py
@@ -15,3 +15,4 @@ Temporary commit content for 2022-12-02T16:01:07
 Temporary commit content for 2023-01-01T16:01:07
 Temporary commit content for 2023-01-03T16:01:07
 Temporary commit content for 2023-01-15T16:01:07
+Temporary commit content for 2023-01-18T16:01:07
