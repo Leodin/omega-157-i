7fdf0bc93137a49391d4b600fb2b496dd76344e1
Leodin <thornleodin@yahoo.com>
2023-06-05 16:01:07 +0100

    Fixed typos in documentation

--- script.ps1
+++ script.ps1
@@ -27,3 +27,4 @@ Temporary commit content for 2023-05-07T16:01:07
 Temporary commit content for 2023-05-23T16:01:07
 Temporary commit content for 2023-05-24T16:01:07
 Temporary commit content for 2023-05-30T16:01:07
+Temporary commit content for 2023-06-05T16:01:07
