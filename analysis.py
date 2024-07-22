291c9302e6132d274cd6d38ed95d49b0f1094702
Leodin <thornleodin@yahoo.com>
2024-07-22 16:01:07 +0100

    Fixed typos in documentation

--- analysis.py
+++ analysis.py
@@ -72,3 +72,4 @@ Temporary commit content for 2024-06-18T16:01:07
 Temporary commit content for 2024-06-22T16:01:07
 Temporary commit content for 2024-07-09T16:01:07
 Temporary commit content for 2024-07-15T16:01:07
+Temporary commit content for 2024-07-22T16:01:07
