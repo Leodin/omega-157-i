eabad39664e05743d23fc220e81ea359ce41a28d
Leodin <thornleodin@yahoo.com>
2025-01-24 16:01:07 +0100

    Added validation for user input

--- analysis.py
+++ analysis.py
@@ -92,3 +92,4 @@ Temporary commit content for 2024-11-28T16:01:07
 Temporary commit content for 2024-12-13T16:01:07
 Temporary commit content for 2025-01-09T16:01:07
 Temporary commit content for 2025-01-21T16:01:07
+Temporary commit content for 2025-01-24T16:01:07
